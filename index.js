ae09e591c19b2b68b1a8d45b17061f74a5aec1bf
Sibulele Ngada <sibulelengada@gmail.com>
2024-10-03 12:40:58 +0200

    Import helper functions and initial data into index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // TASK: import helper functions from utils
+import taskFunctions from "./utils/taskFunctions"
 // TASK: import initialData
+import initialData from "./initialData"
 
 
 /*************************************************************************************************************************************************
